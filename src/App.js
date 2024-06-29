1fdce1d3187fd3da60539592e10ec05b2b19bf60
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-06-29 15:14:33 -0400

    React hook - useState example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClickIncrease = () => {
+    setCount(count + 1);
+  };
+  const handleClickDecrease = () => {
+    setCount(count - 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {count}
+      <br />
+      <button onClick={handleClickIncrease}>increase by 1</button>
+      <br />
+      <button onClick={handleClickDecrease}>decrease by 1</button>
     </div>
   );
 }
